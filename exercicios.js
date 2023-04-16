2e023431facfee2a8c26361b42ba84c45c2396d1
kelvin369 <99517360+kelvin369@users.noreply.github.com>
2023-04-16 06:56:53 -0300

    finished exercise

--- exercicios.js
+++ exercicios.js
@@ -16,3 +16,26 @@ let atletas = [
       notas: [10, 10, 10, 9, 9.5]
     }
    ];
+
+
+   function calcularMedia(atletas) {
+    for (let i = 0; i < atletas.length; i++) {
+      let notas = atletas[i].notas;
+      notas = notas.sort();
+      notasComputadas = notas.slice(1, 4);
+
+      let soma = 0;
+      
+      notasComputadas.forEach(nota => {
+        soma += nota;
+      });
+
+      let media = soma / notasComputadas.length;
+      console.log(`Atleta: ${atletas[i].nome}`);
+      console.log(`Notas Obtidas: ${notas}`);
+      console.log(`Média Válida: ${media.toFixed(2)}\n`);
+    }
+
+  }
+   
+   calcularMedia(atletas);
